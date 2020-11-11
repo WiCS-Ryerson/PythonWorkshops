5a6cd89bf1ffecf7895f93174388c12f797d15c2
manpreet rajpal <manpreetrajpal7@gmail.com>
2020-11-10 19:26:51 -0500

    first commit

---
+++ pythonWorkshop1/Input.py
@@ -0,0 +1,23 @@
+
+# Developers often have a need to interact with users, either to get data or to provide some sort of result. 
+
+# Most programs today use a dialog box as a way of asking the user to provide some type of input.
+
+
+# input ()
+
+# input ( ) : This function first takes the input from the user and then evaluates the expression, which means Python automatically identifies whether user entered a string or a number or list. 
+
+# If the input provided is not correct then either syntax error or exception is raised by python.
+
+# For Example:
+ 
+# number=input("Enter a number: ")
+# print(number)
+
+# name=input("Enter your name : ")
+# print(name)
+
+# print("type of number: ", type(number))
+
+# print("type of name : ", type(name))
---
+++ pythonWorkshop1/Variables.py
@@ -0,0 +1,14 @@
+
+#  Variables are containers for storing data values.
+
+# A variable is created the moment you first assign a value to it.
+
+# Variable names can have uppercase letters, lowercase letters, digits, and _  but can not start with digit
+
+# Naming a varible is important.
+
+#  For Example:
+# 
+# 
+# a=3
+# print(a)
